685db0a1d2b2d43a66052d9b325b7530da16ef8f
Anna Bekkarevich <annabekkarevich@gmail.com>
2019-07-22 09:42:01 -0400

    feat: Add ARIA attributes

--- js/sw-reg.js
+++ js/sw-reg.js
@@ -4,4 +4,6 @@
 if (navigator.serviceWorker) {
     // Register service worker
     navigator.serviceWorker.register('sw.js')
+        .then(reg => console.log('ServiceWorker registration successful with scope: ', reg.scope))
+        .catch(err => console.log('ServiceWorker registration failed: ', err))
 }
